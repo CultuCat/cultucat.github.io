eeaccfe1f2ebc279c37314030419002635e8c170
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-10-21 19:28:23 +0200

    feat: events list view
    route (/admin/events)

---
+++ src/components/itemPreview.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card class="my-2 mx-3" elevation="6">
+    <v-card-title @click="method" class="clickable">
+      <v-btn
+        icon="mdi-chevron-right"
+        variant="plain"
+        :ripple="false"
+        class="pb-1"
+      ></v-btn>
+      <strong>{{ item.name }}</strong>
+      <p class="dates">
+        <template v-if="item.startDate != item.endDate">
+        ({{ item.startDate }} - {{ item.endDate }})
+      </template>
+      <template v-else>
+        ({{ item.startDate }})
+      </template>
+      </p>
+      
+      <v-divider></v-divider>
+    </v-card-title>
+    <v-card-text class="mx-8">
+      {{ item.description }}
+      <br /><br /><strong>Price: {{ item.price }}.</strong>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    method() {
+      console.log("clicked");
+    },
+    extractPrice(priceString) {
+      const regex = /(\d+)\s*€/;
+      const match = priceString.match(regex);
+
+      if (match) {
+        // Si se encontró un número, devolverlo como un número entero
+        return parseInt(match[1], 10);
+      } else if (priceString.trim().toLowerCase() === "free") {
+        // Si no se encontró un número y el precio es "Free", devolver 0 para indicar que es gratuito
+        return 0;
+      } else {
+        // Si no se encontró un número y no es "Free", devolver un valor predeterminado o NaN
+        return NaN;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.clickable:hover {
+  cursor: pointer;
+}
+
+.dates{
+    display: inline-block;
+    font-size: 0.6em;
+    margin-left: 1.5rem;
+}
+</style>
---
+++ src/components/listOfItems.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-col>
+    <v-container class="d-flex justify-center align-center">
+      <v-col cols="10" md="10" sm="12">
+        <v-card>
+          <v-list>
+            <v-list-item v-for="tag in tagCategories" :key="tag">
+              <v-card variant="flat" class="my-2">
+                <v-card-item>
+                  <div class="text-h6">
+                    <strong>{{
+                      tag.charAt(0).toUpperCase() + tag.slice(1)
+                    }}</strong>
+                  </div>
+                  <template v-slot:append>
+                    <v-btn
+                      append-icon="mdi-chevron-right"
+                      variant="plain"
+                      rounded="xl"
+                      >View all</v-btn
+                    >
+                  </template>
+                </v-card-item>
+                <v-divider class="mb-8"></v-divider>
+                <v-list>
+                  <v-list-item
+                    v-for="item in filtereditems(tag)"
+                    :key="item.id"
+                  >
+                    <itemPreview :item="item" />
+                  </v-list-item>
+                </v-list>
+              </v-card>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-container>
+  </v-col>
+</template>
+
+<script setup>
+import itemPreview from "@/components/itemPreview.vue";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      tagCategories: ["music", "art", "films", "theatre"],
+    };
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    method() {
+      console.log("clicked");
+    },
+    filtereditems(tag) {
+      const matchingitems = this.items.filter((item) =>
+        item.tags.includes(tag)
+      );
+      // Limita la cantidad de elementos a un máximo de 3
+      return matchingitems.slice(0, 3);
+    },
+  },
+  components: {
+    itemPreview,
+  },
+};
+</script>
+
+<style scoped>
+.clickable:hover {
+  cursor: pointer;
+}
+</style>
