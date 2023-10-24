c7599d50d94a6f8e749dba84db58a34d7866c4cc
Ericriera <ericrp2002@gmail.com>
2023-10-24 18:07:51 +0200

    login view

---
+++ src/components/loginBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-img class="ma-5" src="../assets/full_logo.png" width="250" style="position:absolute;"></v-img>
+  <v-container class="custom-fill-height">
+    <v-card-text class="d-flex flex-column justify-center">
+      <div class="text-h4 text-left">Benvingut</div>
+      <div class="text-subtitle-1">Sisplau inicia sesió amb google per poder accedir</div>
+      <!-- <v-btn class="pa-0 ma-16 text-none" height="39.4px" width="216.2px">
+      </v-btn> -->
+      <v-btn @click="login">Login Using Google</v-btn>
+    </v-card-text>
+    <router-link to="home">
+      <v-btn rounded="lg" color="white" :style="{ backgroundColor: '#68ceec' }">Log in</v-btn>
+    </router-link>
+  </v-container>
+</template>
+  
+<script>
+export default {
+  name: "loginBar",
+  components: {
+  },
+};
+</script>
+  
+<style lang="scss" scoped>
+.subtitle {
+  color: $terciary;
+}
+
+.custom-fill-height {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,38 @@
 <template>
-  <router-link to="home">
-    <v-btn rounded="lg" color="white" :style="{ backgroundColor: '#68ceec' }">
-      Log in
-    </v-btn>
-  </router-link>
+  <v-row>
+    <v-col class="pb-0" v-if="windowWidth > 960">
+      <v-img height="100vh" cover src="../assets/login.jpg"></v-img>
+    </v-col>
+    <v-col class="pb-0">
+      <loginBar />
+    </v-col>
+  </v-row>
 </template>
 
+
 <script>
+import loginBar from "@/components/loginBar.vue";
+
 export default {
   name: "Login",
+  components: {
+    loginBar
+  },
+  data() {
+    return {
+      windowWidth: window.innerWidth
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.windowWidth = window.innerWidth;
+    }
+  }
 }
 </script>
