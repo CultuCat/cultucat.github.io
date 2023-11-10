c87babd0b6dd7af0ac72dedf137ffb23375c0382
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-11-10 16:50:54 +0100

    fix: https host propio

--- src/views/admin/events.vue
+++ src/views/admin/events.vue
@@ -24,7 +24,7 @@ export default {
     return {
       itemsJSON: [],
       loading: false,
-      urlToFetch: "http://54.82.9.22:8000/events/?ordering=-dataIni",
+      urlToFetch: "https://cultucat.hemanuelpc.es/events/?ordering=-dataIni",
       //array de eventos generado aleatoriamente
       /*items: [
         {
