48d9df55d8ab88a34420543810517a3725c002f4
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-11-19 14:57:19 +0100

    fix: sonarcloud vulnerability

--- src/components/commentForm.vue
+++ src/components/commentForm.vue
@@ -1,45 +1,53 @@
 <template>
-    <v-form @submit.prevent="submitComment">
-        <v-text-field v-model="comment" label="Comment" required variant="outlined"></v-text-field>
-        <v-btn type="submit" color="primary">Send Comment</v-btn>
-    </v-form>
+  <v-form @submit.prevent="submitComment">
+    <v-text-field
+      v-model="comment"
+      label="Comment"
+      required
+      variant="outlined"
+    ></v-text-field>
+    <v-btn type="submit" color="primary">Send Comment</v-btn>
+  </v-form>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            formData: {
-                text: 0,
-                event: "",
+  data() {
+    return {
+      formData: {
+        text: 0,
+        event: "",
+      },
+      comment: "",
+    };
+  },
+  methods: {
+    async submitComment() {
+      this.formData.text = this.comment;
+      this.formData.event = this.$route.params.event_id;
+
+      try {
+        const response = await fetch(
+          "https://cultucat.hemanuelpc.es/comments/",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
             },
-            comment: "",
-        };
-    },
-    methods: {
-        async submitComment() {
-            this.formData.text = this.comment;
-            this.formData.event = this.$route.params.event_id;
+            body: JSON.stringify(this.formData),
+          }
+        );
 
-            try {
-                const response = await fetch('http://cultucat.hemanuelpc.es/comments/', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(this.formData),
-                });
-
-                if (response.ok) {
-                    this.$emit('comment-posted');
-                    this.comment = '';
-                } else {
-                    console.error('Error al enviar comentario');
-                }
-            } catch (error) {
-                console.error('Error en la solicitud POST:', error);
-            }
-        },
+        if (response.ok) {
+          this.$emit("comment-posted");
+          this.comment = "";
+        } else {
+          console.error("Error al enviar comentario");
+        }
+      } catch (error) {
+        console.error("Error en la solicitud POST:", error);
+      }
     },
+  },
 };
 </script>
