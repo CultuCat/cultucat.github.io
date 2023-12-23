8a3c1af5d6e3b33a674878928b7f71d827b96036
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-12-23 19:32:09 +0100

    fix: free events dont have discounts

--- src/components/buy.vue
+++ src/components/buy.vue
@@ -3,73 +3,74 @@
 <!-- ======================================================================= -->
 
 <template>
-                <v-card elevation="4">
-                    <v-toolbar color="#ff6961" dark>
-                        <v-icon size="35" class="ml-6">mdi-ticket</v-icon>
-                        <v-toolbar-title>Purchase summary</v-toolbar-title>
-                        <v-spacer></v-spacer>
-                        <v-toolbar-items>
-                            <v-btn icon dark variant="plain" @click="cancel">
-                                <v-icon>mdi-close</v-icon>
-                            </v-btn>
-                        </v-toolbar-items>
-                    </v-toolbar>
-                    <v-card-text>
-                        <v-list >
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-title><strong>Event name:</strong></v-list-item-title>
-                                <v-list-item-subtitle>{{ eventInfo.nom }}</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
+    <v-card elevation="4">
+        <v-toolbar color="#ff6961" dark>
+            <v-icon size="35" class="ml-6">mdi-ticket</v-icon>
+            <v-toolbar-title>Purchase summary</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn icon dark variant="plain" @click="cancel">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-card-text>
+            <v-list>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title><strong>Event name:</strong></v-list-item-title>
+                        <v-list-item-subtitle>{{ eventInfo.nom }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
 
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-title><strong>Date:</strong></v-list-item-title>
-                                <v-list-item-subtitle>{{ transformDate(eventInfo.dataIni) }}</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title><strong>Date:</strong></v-list-item-title>
+                        <v-list-item-subtitle>{{ transformDate(eventInfo.dataIni) }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
 
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-title><strong>Place:</strong></v-list-item-title>
-                                <v-list-item-subtitle>{{ eventInfo.espai }}</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title><strong>Place:</strong></v-list-item-title>
+                        <v-list-item-subtitle>{{ eventInfo.espai }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
 
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-title><strong>Price:</strong></v-list-item-title>
-                                <v-list-item-subtitle>{{ eventInfo.preu }}€</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-title><strong>Discount:</strong></v-list-item-title>
-                                <v-select v-if="discounts.length > 0 && filteredDiscounts.length > 0"
-                                    v-model="selectedDiscount" :items="filteredDiscounts" :item-props="itemProps"
-                                    density="compact" label="Choose a discount" clearable>
-                                </v-select>
-                                <v-alert v-else value="true" type="warning" class="mt-2">
-                                    No discounts available.
-                                </v-alert>
-                            </v-list-item-content>
-                        </v-list-item>
-                                <v-list-item class="mt-0">
-                                    <v-list-item-content>
-                                        <v-list-item-title><strong>Total:</strong></v-list-item-title>
-                                        <v-list-item-subtitle>{{ isNaN(calculateTotalPrice) ? "Non price" : calculateTotalPrice + '€' }}</v-list-item-subtitle>
-                                    </v-list-item-content>
-                                </v-list-item>
-                                
-                    </v-list>
-                    </v-card-text>
-                    <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn @click="cancel">Cancel</v-btn>
-                                    <v-btn @click="buy(selectedDiscount)" :loading="buyLoading" color="success">Buy</v-btn>
-                                </v-card-actions>
-                </v-card>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title><strong>Price:</strong></v-list-item-title>
+                        <v-list-item-subtitle>{{ eventInfo.preu == "Gratuït" ? "Gratuït" : eventInfo.preu +
+                            '€' }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item v-if="eventInfo.preu != 'Gratuït'">
+                    <v-list-item-content>
+                        <v-list-item-title><strong>Discount:</strong></v-list-item-title>
+                        <v-select v-if="discounts.length > 0 && filteredDiscounts.length > 0" v-model="selectedDiscount"
+                            :items="filteredDiscounts" :item-props="itemProps" density="compact" label="Choose a discount"
+                            clearable>
+                        </v-select>
+                        <v-alert v-else value="true" type="warning" class="mt-2">
+                            No discounts available.
+                        </v-alert>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item v-if="eventInfo.preu != 'Gratuït'" class="mt-0">
+                    <v-list-item-content>
+                        <v-list-item-title><strong>Total:</strong></v-list-item-title>
+                        <v-list-item-subtitle>{{ isNaN(calculateTotalPrice) ? "Non price" : calculateTotalPrice + '€'
+                        }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-card-text>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="cancel">Cancel</v-btn>
+            <v-btn @click="buy(selectedDiscount)" :loading="buyLoading" color="success">Buy</v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
   
   <!-- =============================== SCRIPTS =============================== -->
