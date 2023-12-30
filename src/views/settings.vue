e06aa49f7d17fdf2a38a3aeeacecf80546b2d417
Eric Riera <ericrp2002@gmail.com>
2023-12-30 13:10:49 +0100

    add: settings view

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1,7 +1,15 @@
 <template>
-  <HelloWorld />
+  <h1 class="mt-4 ml-5" style="color: #ff6961">Settings</h1>
+  <v-card class="mx-5 mt-4 card" rounded="lg" elevation="4">
+    <p>Aquí hi haurà settings</p>
+  </v-card>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+<script>
 </script>
+
+<style scoped lang="scss">
+.card {
+  height: calc(100vh - 95px);
+}
+</style>
