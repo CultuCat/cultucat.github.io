8a54e646751cff350af984926c1470e689a7a487
Marc Román <marc.roman.quilez@estudiantat.upc.edu>
2024-01-12 14:21:43 +0100

    fix: routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    name: 'page',
-    component: () => import('@/views/page.vue'),
+    redirect: '/login',
   },
   {
     path: '/login',
